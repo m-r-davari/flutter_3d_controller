2d42698c7617da8f10c68f0a1b2d51171a885ca0
Faraz <faraz@Farazs-MacBook-Pro.local>
2025-03-11 01:47:19 +0330

    added macos example transparent bg color

--- example/macos/Runner/AppDelegate.swift
+++ example/macos/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import FlutterMacOS
+import flutter_inappwebview_macos
 
 @main
 class AppDelegate: FlutterAppDelegate {
@@ -10,4 +11,17 @@ class AppDelegate: FlutterAppDelegate {
   override func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
     return true
   }
+}
+
+extension InAppWebView {
+    @objc public override func viewDidMoveToWindow() {
+        super.viewDidMoveToWindow()
+
+        if window != nil {
+            print("InAppWebView moved to window, enforcing transparency")
+            self.setValue(false, forKey: "opaque")
+            self.setValue(false, forKey: "drawsBackground")
+            self.layer?.backgroundColor = NSColor.clear.cgColor
+        }
+    }
 }
